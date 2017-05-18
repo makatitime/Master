87c0596029805a7f0fe60085e0e59b6c671875fe
makatitime <93118973@qq.com>
2017-05-18 17:16:39 +0800

    add
    little

--- play/player/forms.py
+++ play/player/forms.py
@@ -2,4 +2,6 @@
 from django import  forms
 
 class ArtForms(forms.Form):
-    your_name = forms.CharField(label='作者', max_length=100)
+    name = forms.CharField(label='作者', max_length=100)
+    title = forms.CharField(label='标题', max_length=100)
+    content = forms.CharField(label='',widget=forms.Textarea)
--- play/player/views.py
+++ play/player/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from  django.http import HttpResponseRedirect
 from .models import articles
 from .forms import ArtForms
+import hashlib
 # Create your views here.
 # def index_page(request):
 #     d = request.META
@@ -16,12 +17,36 @@ def index_page(request):
 
 def add_page(request):
     if request.method == 'POST':
-        at = request.POST['author']
-        tt = request.POST['title']
-        cc = request.POST['content']
+        form = ArtForms(request.POST)
+       # print form
+        at = form.cleaned_data['name']
+        tt = form.cleaned_data['title']
+        cc = form.cleaned_data['content']
         ars = articles.objects.create(author=at,title=tt,content=cc)
         ars.save()
         return HttpResponseRedirect("/")
     else:
         forms = ArtForms()
-        return render(request,'add.html',{'form':forms})
+        return render(request,'add.html',{'form':forms.as_p()})
+
+def register(request):
+    if request.method == 'POST':
+        form = AddForm(request.POST)
+
+        if form.is_valid():
+            m = hashlib.md5()
+            username  = form.cleaned_data['username']
+            password = hashlib.md5(form.cleaned_data['password'])
+            password = password.hexdigest()
+            email = form.cleaned_data['Email']
+            gender = form.cleaned_data['Gender']
+            age = 23
+            res = Author.objects.create(username=username,password=password,age=age,sex=gender)
+            res.save
+            return HttpResponse(password)
+        else:
+            print 'hehe'
+
+    else:
+        form = AddForm()
+        return  render(request,'register.html',{'form': form })
