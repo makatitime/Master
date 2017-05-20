ba241deba48f5f0028b1c146ec47ea816865c6ec
makatitime <93118973@qq.com>
2017-05-20 10:31:24 +0800

    T
    T

--- play/player/forms.py
+++ play/player/forms.py
@@ -5,3 +5,4 @@ class ArtForms(forms.Form):
     name = forms.CharField(label='作者', max_length=100)
     title = forms.CharField(label='标题', max_length=100)
     content = forms.CharField(label='',widget=forms.Textarea)
+
