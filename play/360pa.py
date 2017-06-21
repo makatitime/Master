e2493b365805e34a569d5293cd600a5d85d21b4e
1 <1>
2017-06-21 08:38:38 +0800

    360  spider
    simulate  360 Spider

---
+++ play/360pa.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import urllib
+import urllib2
+import re
+url = 'http://bbs.meadin.com/thread-365344280-1-1.html'
+agent = 'Mozilla/5.0 (NT 5.1; zh-CN; rv:1.8.0.11) Gecko/20070312 Firefox/1.5.0.11; 360Spider'
